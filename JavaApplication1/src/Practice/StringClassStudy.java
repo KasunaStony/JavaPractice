53e90031f0ad4062fda29c8ca6ad1a888977b5aa
Ruiling Ma <maruiling@10.96.179.101>
2018-09-17 10:34:06 -0400

    update

---
+++ JavaApplication1/src/Practice/StringClassStudy.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practice;
+
+/**
+ *
+ * @author maruiling
+ */
+import java.util.*;
+import java.io.*;
+        
+public class StringClassStudy {
+    
+    public static void main(String args[]){
+        
+        String s = "aew rte,wer";
+        String s1 = "A";
+        
+       // System.out.println("fjiewoj" + "\n");
+        StringTokenizer st = new StringTokenizer(s, "e ,");
+        
+        System.out.println(s1.substring(s1.length()));
+        //while(st.hasMoreElements()){
+           // System.out.println();
+                 
+       // }
+        
+    }
+    
+}
