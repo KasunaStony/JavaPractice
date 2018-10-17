b36d49ccfe40572806cd297ff619439d8cfe6d87
Ruiling Ma <maruiling667216@gmail.com>
2018-10-17 08:59:22 -0400

    update

--- JavaApplication1/src/USACOPractice/milk2.java
+++ JavaApplication1/src/USACOPractice/milk2.java
@@ -1,9 +1,11 @@
+
+
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package USACOPractice;
+ID: maruili1
+LANG: JAVA
+TASK: milk2
+*/
+
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -22,7 +24,7 @@ public class milk2 {
     
     public static boolean ifOverLap(int cTail, int nBegin){
         
-        return cTail > nBegin;
+        return cTail >= nBegin;
     }
     
     public static void main(String args[]) throws FileNotFoundException, IOException{
@@ -45,33 +47,39 @@ public class milk2 {
             
         }
         
-        for(int i = 0; i < n; i++){
-            
-            System.out.println(data[i][0] + " " + data[i][1]);
-            
-        }
+        
         
         Arrays.sort(data, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        for(int i = 0; i < n; i++){
+            System.out.println(data[i][0] + " " + data[i][1]);
+        }
         
         int head = data[0][0];
         int tail = data[0][1];
         
-        int longest = 0;
+        int longestC = data[0][1] - data[0][0];
+        int longestD = 0;
         
         for(int i = 1; i < n; i++){
             
             if(ifOverLap(tail, data[i][0])){
-                tail = data[i][1];
+                if(data[i][1] > tail)
+                    tail = data[i][1];                
             }else{
-                if((tail - head) > longest) longest = tail - head;
+                if((tail - head) > longestC) longestC = tail - head;
+                if((data[i][0] - tail) > longestD) longestD = data[i][0] - tail;
                 head = data[i][0];
                 tail = data[i][1];
             }
             
         }
         
-        System.out.println(longest);
         
+        writer.write(longestC + " " + longestD + "\n");
+
+        reader.close();
+        writer.close();
         
         
     }
